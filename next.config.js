4f32dd1c70d63da4ab8905ab2db3a01cbfb14247
knakul_853 <nakul.bharti@gsat.us>
2024-11-15 19:29:53 +0530

    Deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true,
+    remotePatterns: [],
   },
   assetPrefix: isProd ? "/portfolio/" : "",
   basePath: isProd ? "/portfolio" : "",
   output: "export",
+  trailingSlash: true,
   // Handle hydration errors by suppressing specific warnings
   onDemandEntries: {
     // This will help reduce client/server mismatches
