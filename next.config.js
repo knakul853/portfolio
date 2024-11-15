60c3244589644b757e742525e26b499be8ca2629
knakul_853 <nakul.bharti@gsat.us>
2024-11-15 15:36:11 +0530

    i don't know what i did

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  output: "export",
+  images: {
+    unoptimized: true, // disables Image Optimization for GitHub Pages compatibility
+  },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+};
